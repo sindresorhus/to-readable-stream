fad779c95328ac828cb16de6594efaa9d34764b8
Richie Bendall <richiebendall@gmail.com>
2022-10-27 03:32:49 +1300

    Require Node.js 18 and return a web stream instead of a Node.js stream (#14)

--- index.js
+++ index.js
@@ -1,10 +1,8 @@
-import {Readable as ReadableStream} from 'node:stream';
-
 export default function toReadableStream(value) {
 	return new ReadableStream({
-		read() {
-			this.push(value);
-			this.push(null);
+		start(controller) {
+			controller.enqueue(value);
+			controller.close();
 		}
 	});
 }
--- index.test-d.ts
+++ index.test-d.ts
@@ -1,5 +1,7 @@
+import {Buffer} from 'node:buffer';
 import toReadableStream from './index.js';
+import {expectType} from 'tsd';
 
-toReadableStream('🦄🌈').pipe(process.stdout);
-toReadableStream(Buffer.from('🦄🌈')).pipe(process.stdout);
-toReadableStream(new Uint8Array(Buffer.from('🦄🌈').buffer)).pipe(process.stdout);
+expectType<ReadableStream<string>>(toReadableStream('🦄🌈'));
+expectType<ReadableStream<Buffer>>(toReadableStream(Buffer.from('🦄🌈')));
+expectType<ReadableStream<Uint8Array>>(toReadableStream(new Uint8Array(Buffer.from('🦄🌈').buffer)));
